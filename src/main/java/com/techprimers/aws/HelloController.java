336f890f79e5177eed38dd7831e69e89def82899
pravallikamedasani <40370835+pravallikamedasani@users.noreply.github.com>
2019-12-13 09:48:43 -0800

    Update HelloController

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello TechPrimers";
+        return "Welcome to Devops Demo IDbyDNA team!";
     }
 }
